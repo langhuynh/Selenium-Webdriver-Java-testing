0744bb0aec14d6d21b32168031ef8c15f36dc2a5
langhuynh <ithuynhlang@gmail.com>
2024-02-17 19:20:29 +1000

    update 1

---
+++ selenium/webdriver/Topic_05_Run_More_Browser.java
@@ -0,0 +1,45 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_05_Run_More_Browser {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+
+
+    @Test
+    public void Run_On_Firefox() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+    }
+    @Test
+    public void Run_On_Chrome() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+    }
+    @Test
+    public void Run_On_Edge() {
+        driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+    }
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
